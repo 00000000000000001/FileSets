4da6f32d5f3fc4ef7fd43978e5658424a308745c
derjo <jonas@SH-Jonas-Mager.localdomain>
2024-03-18 19:09:53 +0100

    added tooltips

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -32,6 +32,7 @@ import javax.swing.JTextField;
 import javax.swing.JViewport;
 import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
+import javax.swing.ToolTipManager;
 
 import mvc.FileSetsModel;
 import mvc.SetController;
@@ -40,284 +41,284 @@ import mvc.SetView;
 import parser.Parser;
 
 public class GUI extends JFrame {
-	private static final long serialVersionUID = 1L;
-	private static GUI gui;
-	Dimension screenSize;
+    private static final long serialVersionUID = 1L;
+    private static GUI gui;
+    Dimension screenSize;
 
-	private static JTabbedPane tabbedPane;
-	private static JTextField eval_textfield;
-	private static JTextArea result_TextArea;
-	private static JLabel bottom_progessBar;
+    private static JTabbedPane tabbedPane;
+    private static JTextField eval_textfield;
+    private static JTextArea result_TextArea;
+    private static JLabel bottom_progessBar;
 
 //	private static String exportPath = "./Export";
-	
-	
 
-	/** Returns an ImageIcon, or null if the path was invalid. */
-	protected ImageIcon createImageIcon(String path, String description) {
-		java.net.URL imgURL = getClass().getResource(path);
-		if (imgURL != null) {
-			return new ImageIcon(imgURL, description);
-		} else {
-			System.err.println("Couldn't find file: " + path);
-			return null;
-		}
-	}
+    /** Returns an ImageIcon, or null if the path was invalid. */
+    protected ImageIcon createImageIcon(String path, String description) {
+        java.net.URL imgURL = getClass().getResource(path);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL, description);
+        } else {
+            System.err.println("Couldn't find file: " + path);
+            return null;
+        }
+    }
 
-	protected JComponent makeTextPanel(String text) {
-		JPanel panel = new JPanel(false);
-		JLabel filler = new JLabel(text);
-		filler.setHorizontalAlignment(JLabel.CENTER);
-		panel.setLayout(new GridLayout(1, 1));
-		panel.add(filler);
-		return panel;
-	}
+    protected JComponent makeTextPanel(String text) {
+        JPanel panel = new JPanel(false);
+        JLabel filler = new JLabel(text);
+        filler.setHorizontalAlignment(JLabel.CENTER);
+        panel.setLayout(new GridLayout(1, 1));
+        panel.add(filler);
+        return panel;
+    }
 
-	private String open() throws NoSuchAlgorithmException, IOException {
-		final JFileChooser chooser = new JFileChooser("Verzeichnis wählen");
-		chooser.setDialogType(JFileChooser.OPEN_DIALOG);
-		chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-		chooser.setFileHidingEnabled(false);
-		final File file = new File("~/");
+    private String open() throws NoSuchAlgorithmException, IOException {
+        final JFileChooser chooser = new JFileChooser("Verzeichnis wählen");
+        chooser.setDialogType(JFileChooser.OPEN_DIALOG);
+        chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+        chooser.setFileHidingEnabled(false);
+        final File file = new File("~/");
 
-		chooser.setCurrentDirectory(file);
-		
-		chooser.addPropertyChangeListener(new PropertyChangeListener() {
-		public void propertyChange(PropertyChangeEvent e) {
-			if (e.getPropertyName().equals(JFileChooser.SELECTED_FILE_CHANGED_PROPERTY)
-					|| e.getPropertyName().equals(JFileChooser.DIRECTORY_CHANGED_PROPERTY)) {
-				final File f = (File) e.getNewValue();
-			}
-		}
-	});
+        chooser.setCurrentDirectory(file);
 
-		chooser.setVisible(true);
-		final int result = chooser.showOpenDialog(null);
+        chooser.addPropertyChangeListener(new PropertyChangeListener() {
+            public void propertyChange(PropertyChangeEvent e) {
+                if (e.getPropertyName().equals(JFileChooser.SELECTED_FILE_CHANGED_PROPERTY)
+                        || e.getPropertyName().equals(JFileChooser.DIRECTORY_CHANGED_PROPERTY)) {
+                    final File f = (File) e.getNewValue();
+                }
+            }
+        });
 
-		if (result == JFileChooser.APPROVE_OPTION) {
-			File inputVerzFile = chooser.getSelectedFile();
-			String inputVerzStr = inputVerzFile.getPath();
+        chooser.setVisible(true);
+        final int result = chooser.showOpenDialog(null);
 
-			return inputVerzStr;
-		}
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File inputVerzFile = chooser.getSelectedFile();
+            String inputVerzStr = inputVerzFile.getPath();
 
-		chooser.setVisible(false);
+            return inputVerzStr;
+        }
 
-		return null;
-	}
+        chooser.setVisible(false);
 
-	private GUI() {
-		super();
+        return null;
+    }
 
-		screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		this.setSize(screenSize.width / 2, screenSize.height / 2);
+    private GUI() {
+        super();
 
-		Container pane = this.getContentPane();
-		/*
-		 * Upper part
-		 */
-		JPanel ceiling_pane = new JPanel();
-		ceiling_pane.setLayout(new BoxLayout(ceiling_pane, BoxLayout.LINE_AXIS));
-		pane.add(ceiling_pane, BorderLayout.PAGE_START);
+        ToolTipManager.sharedInstance().setDismissDelay(10000);
 
-		JButton button_open = new JButton("open");
-		ceiling_pane.add(button_open);
-		button_open.addActionListener(new ActionListener() {
+        screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setSize(screenSize.width / 2, screenSize.height / 2);
 
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// your actions
-				SwingUtilities.invokeLater(() -> {
+        Container pane = this.getContentPane();
+        /*
+         * Upper part
+         */
+        JPanel ceiling_pane = new JPanel();
+        ceiling_pane.setLayout(new BoxLayout(ceiling_pane, BoxLayout.LINE_AXIS));
+        pane.add(ceiling_pane, BorderLayout.PAGE_START);
 
-					try {
+        JButton button_open = new JButton("open");
+        ceiling_pane.add(button_open);
+        button_open.addActionListener(new ActionListener() {
 
-						String path = open();
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // your actions
+                SwingUtilities.invokeLater(() -> {
 
-						// Create new Tab
-						// set tabs name to name of new set
-						String name = name(FileSetsModel.getInstance().getList().size());
-						addTab(name);
+                    try {
 
-						// select new tab
-						tabbedPane.setSelectedIndex(tabbedPane.getComponentCount() - 1);
+                        String path = open();
 
-						// set text on tab
-						int count = tabbedPane.getTabCount();
-						JScrollPane scrollPane = (JScrollPane) tabbedPane.getComponent(count - 1);
-						JViewport viewport = scrollPane.getViewport();
-						JTextArea textArea = (JTextArea) viewport.getView();
-						textArea.setText(path);
+                        // Create new Tab
+                        // set tabs name to name of new set
+                        String name = name(FileSetsModel.getInstance().getList().size());
+                        addTab(name);
 
-						// initializing mvc
-						
-						SetController setController = new SetController();
-						
-						// set name of new set
-						setController.getSetModel().setName(name);
-						
+                        // select new tab
+                        tabbedPane.setSelectedIndex(tabbedPane.getComponentCount() - 1);
 
-						SetView setView = new SetView(textArea);
+                        // set text on tab
+                        int count = tabbedPane.getTabCount();
+                        JScrollPane scrollPane = (JScrollPane) tabbedPane.getComponent(count - 1);
+                        JViewport viewport = scrollPane.getViewport();
+                        JTextArea textArea = (JTextArea) viewport.getView();
+                        textArea.setText(path);
 
-						setView.subscribe(setController.getSetModel());
-						setController.getSetModel().addView(setView);
+                        // initializing mvc
 
-						// set new list causes update()
-						List<SetController> list = FileSetsModel.getInstance().getList();
-						list.add(setController);
-						FileSetsModel.getInstance().setList(list);
+                        SetController setController = new SetController();
 
-						// analyse path to new created tab
-						// TODO analyze in a thread
-						Thread thread = new Thread(new SetControllerThread(setController, path));
-						
-						
-						thread.start();
-						
-					} catch (NoSuchAlgorithmException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					} catch (IOException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
+                        // set name of new set
+                        setController.getSetModel().setName(name);
 
-				});
-			}
+                        SetView setView = new SetView(textArea);
 
-			private void addTab(String name) {
-				// TODO Auto-generated method stub
-				JTextArea tabbedPane_TextArea = new JTextArea();
-				JScrollPane set_ScrollPane = new JScrollPane(tabbedPane_TextArea);
+                        setView.subscribe(setController.getSetModel());
+                        setController.getSetModel().addView(setView);
 
-				ImageIcon icon = createImageIcon("images/middle.gif", "");
+                        // set new list causes update()
+                        List<SetController> list = FileSetsModel.getInstance().getList();
+                        list.add(setController);
+                        FileSetsModel.getInstance().setList(list);
 
-				tabbedPane.addTab(name, icon, set_ScrollPane, "Does nothing");
-				tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
-			}
+                        // analyse path to new created tab
+                        // TODO analyze in a thread
+                        Thread thread = new Thread(new SetControllerThread(setController, path));
 
-			private String name(int id) {
-				/*
-				 * 0 => A 1 => B ... 25 => Z 26 => AA 27 => BB ... 51 = ZZZ 52 => AAA ...
-				 */
-				char[] alphabet = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
-						'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
-				StringBuilder name = new StringBuilder("");
-				char c = alphabet[(id % 26)];
-				name.append(c);
-				for (int i = 0; i < (id / 26); ++i) {
-					name.append(c);
-				}
-				return name.toString();
-			}
+                        thread.start();
 
-		});
+                    } catch (NoSuchAlgorithmException e1) {
+                        // TODO Auto-generated catch block
+                        e1.printStackTrace();
+                    } catch (IOException e1) {
+                        // TODO Auto-generated catch block
+                        e1.printStackTrace();
+                    }
 
-		ceiling_pane.add(new JSeparator(SwingConstants.VERTICAL));
+                });
+            }
 
-		JPanel ceiling_eval = new JPanel(new java.awt.BorderLayout());
-		ceiling_pane.add(ceiling_eval, BorderLayout.PAGE_START);
-		ceiling_eval.setPreferredSize(new Dimension(250, 30));
+            private void addTab(String name) {
+                // TODO Auto-generated method stub
+                JTextArea tabbedPane_TextArea = new JTextArea();
+                JScrollPane set_ScrollPane = new JScrollPane(tabbedPane_TextArea);
 
-		eval_textfield = new JTextField();
-		ceiling_eval.add(eval_textfield, BorderLayout.CENTER);
+                ImageIcon icon = createImageIcon("images/middle.gif", "");
 
-		JButton eval_button = new JButton("evaluate");
-		ceiling_eval.add(eval_button, BorderLayout.LINE_END);
-		eval_button.addActionListener(new ActionListener() {
+                tabbedPane.addTab(name, icon, set_ScrollPane, "Does nothing");
+                tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
+            }
 
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// Get eval of the parser running
-				
-				
-				Parser parser = new Parser(eval_textfield.getText());
-				parser.scanToken();
-				parser.resultTree = parser.parseS();
-				
-				if (parser.getNextToken() != "") {
-					System.err.println("Input-String fehlerhaft. Enthält: " + parser.getNextToken());
-				}
-				
-				
-				if (parser.resultTree != null) {
-					System.out.println("evaluating : " + parser.resultTree.toString());
-					SetController setController;
-					setController = parser.resultTree.eval();
-					result_TextArea.setText(setController.toString());
-				}
-				
-				
-				
-			}
+            private String name(int id) {
+                /*
+                 * 0 => A 1 => B ... 25 => Z 26 => AA 27 => BB ... 51 = ZZZ 52 => AAA ...
+                 */
+                char[] alphabet = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+                        'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+                StringBuilder name = new StringBuilder("");
+                char c = alphabet[(id % 26)];
+                name.append(c);
+                for (int i = 0; i < (id / 26); ++i) {
+                    name.append(c);
+                }
+                return name.toString();
+            }
 
-		});
+        });
 
-		ceiling_pane.add(new JSeparator(SwingConstants.VERTICAL));
+        ceiling_pane.add(new JSeparator(SwingConstants.VERTICAL));
 
-		JButton button_export = new JButton("export");
-		ceiling_pane.add(button_export);
+        JPanel ceiling_eval = new JPanel(new java.awt.BorderLayout());
+        ceiling_pane.add(ceiling_eval, BorderLayout.PAGE_START);
+        ceiling_eval.setPreferredSize(new Dimension(250, 30));
 
-		/*
-		 * Center part
-		 */
+        eval_textfield = new JTextField();
+        eval_textfield.setToolTipText("<html>Hier können Sie einen logischen Ausdruck eingeben.<br>" +
+                "Mengenbezeichner sind A, B, C etc.<br>" +
+                "Operatoren sind & | - /.<br>" +
+                "Achten Sie darauf, dass A&B nicht funktioniert aber A & B schon.</html>");
+        ceiling_eval.add(eval_textfield, BorderLayout.CENTER);
 
-		tabbedPane = new JTabbedPane();
+        JButton eval_button = new JButton("evaluate");
+        ceiling_eval.add(eval_button, BorderLayout.LINE_END);
+        eval_button.addActionListener(new ActionListener() {
 
-		result_TextArea = new JTextArea();
-		JScrollPane result_ScrollPane = new JScrollPane(result_TextArea);
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Get eval of the parser running
 
-		JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, tabbedPane, result_ScrollPane);
-		splitPane.setOneTouchExpandable(true);
-		splitPane.setDividerLocation(this.getHeight() / 2);
+                Parser parser = new Parser(eval_textfield.getText());
+                parser.scanToken();
+                parser.resultTree = parser.parseS();
 
-		pane.add(splitPane);
+                if (parser.getNextToken() != "") {
+                    System.err.println("Input-String fehlerhaft. Enthält: " + parser.getNextToken());
+                }
 
-		/*
-		 * Lower part
-		 */
-		JPanel bottom = new JPanel(new java.awt.GridLayout(0, 1));
-		pane.add(bottom, BorderLayout.PAGE_END);
+                if (parser.resultTree != null) {
+                    System.out.println("evaluating : " + parser.resultTree.toString());
+                    SetController setController;
+                    setController = parser.resultTree.eval();
+                    result_TextArea.setText(setController.toString());
+                }
 
-		bottom_progessBar = new JLabel("");
-		bottom.add(bottom_progessBar);
+            }
 
-		/*
-		 * 	
-		 */
-		// Create new JFrame with title "FileSets"
-		this.setTitle("FileSets");
+        });
 
-		// exit on close button
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ceiling_pane.add(new JSeparator(SwingConstants.VERTICAL));
 
-		// Wir lassen unseren Frame anzeigen
-		this.setVisible(true);
-	}
+        JButton button_export = new JButton("export");
+        ceiling_pane.add(button_export);
 
-	public static JLabel getBottom_progessBar() {
-		return bottom_progessBar;
-	}
+        /*
+         * Center part
+         */
 
-	public static void setBottom_progessBar(JLabel bottom_progessBar) {
-		GUI.bottom_progessBar = bottom_progessBar;
-	}
+        tabbedPane = new JTabbedPane();
 
-	public static GUI getInstance() {
-		if (gui == null) {
-			return gui = new GUI();
-		} else {
-			return gui;
-		}
-	}
+        result_TextArea = new JTextArea();
+        result_TextArea.setToolTipText(
+                "<html>Nachdem Sie einen gültigen logischen Ausdruck oben in die Eingabezeile"
+                + "<br>eingegeben haben und evaluate betätigt haben erscheint das Ergebnis der Mengenoperation "
+                        + "<br>hier in diesem Textfeld.</html>");
+        JScrollPane result_ScrollPane = new JScrollPane(result_TextArea);
 
-	public static JTabbedPane getTabbedPane() {
-		return tabbedPane;
-	}
+        JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, tabbedPane, result_ScrollPane);
+        splitPane.setOneTouchExpandable(true);
+        splitPane.setDividerLocation(this.getHeight() / 2);
 
-	public static void main(String[] args) {
-		GUI.getInstance();
-	}
+        pane.add(splitPane);
 
+        /*
+         * Lower part
+         */
+        JPanel bottom = new JPanel(new java.awt.GridLayout(0, 1));
+        pane.add(bottom, BorderLayout.PAGE_END);
 
+        bottom_progessBar = new JLabel("");
+        bottom.add(bottom_progessBar);
+
+        /*
+         * 	
+         */
+        // Create new JFrame with title "FileSets"
+        this.setTitle("FileSets");
+
+        // exit on close button
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Wir lassen unseren Frame anzeigen
+        this.setVisible(true);
+    }
+
+    public static JLabel getBottom_progessBar() {
+        return bottom_progessBar;
+    }
+
+    public static void setBottom_progessBar(JLabel bottom_progessBar) {
+        GUI.bottom_progessBar = bottom_progessBar;
+    }
+
+    public static GUI getInstance() {
+        if (gui == null) {
+            return gui = new GUI();
+        } else {
+            return gui;
+        }
+    }
+
+    public static JTabbedPane getTabbedPane() {
+        return tabbedPane;
+    }
+
+    public static void main(String[] args) {
+        GUI.getInstance();
+    }
 
 }
